2783483741ceff5b1243d0e54bbfdddb91f933f0
Manikandan Kannan <k.kannan@nagarro.com>
2022-02-28 09:09:13 +0530

    User management

---
+++ src/Services/UserManagement/UserManagement.API/DbContext/UserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.API.DbContext
+{
+    public class UserDbContext : IdentityDbContext<IdentityUser>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
